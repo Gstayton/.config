d7d7e36118b6474252b1f38f171e00cf714cb0aa
Gstayton <kosan.shikova.com>
2025-09-18 15:26:46 -0700

    Add vim-suda plugin to configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,6 +32,7 @@ vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{ src = "https://github.com/chomosuke/typst-preview.nvim" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/lambdalisue/vim-suda" },
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
